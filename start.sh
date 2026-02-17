ef0342ac8add2252a16534123d741cfb0789c85b
JKMchen <141596304+JKMchen@users.noreply.github.com>
2026-02-18 03:21:49 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 apt update && apt upgrade -y && apt install python3 python-is-python3 python3-pip -y
-pip3 install requests pyTelegramBotAPI fake_useragent itertools --break-system-packages
-pip install requests pyTelegramBotAPI fake_useragent itertools --break-system-packages
-python 1.py
+pip install requests pyTelegramBotAPI fake_useragent --break-system-packages
+python3 1.py
